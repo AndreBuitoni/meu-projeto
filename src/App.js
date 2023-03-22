d0f93a277e47c6047677535330e5f4c695677b76
André Buitoni <a.buitoni@gmail.com>
2023-03-22 17:31:54 -0300

    Finished State Lift Lecture
    New componentes
    - SeuNome.js
    - Saudacao.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import {useState} from 'react'
+
 import './App.css'
-import OutraLista from './components/OutraLista';
+import Saudacao from './components/Saudacao';
+import SeuNome from './components/SeuNome';
 
 function App() {
-
-  const meusItens = ['React', 'Vue', 'Angular']
+  const [nome, setNome] = useState()
 
   return (
     <div className="App">
-      <h1>Renderização de Listas</h1>
-      <OutraLista itens={meusItens} />
-      <OutraLista itens={''} />
+      <h1>State Lift</h1>
+      <SeuNome setNome={setNome} />
+      <Saudacao nome={nome} />
     </div>
   );
 }
