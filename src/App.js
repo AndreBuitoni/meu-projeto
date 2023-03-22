996011425af048c1541955953b6cdd1d199932a3
André Buitoni <a.buitoni@gmail.com>
2023-03-22 13:46:52 -0300

    Created List Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import './App.css';
-import Condicional from './components/Condicional';
+import './App.css'
+import OutraLista from './components/OutraLista';
 
 function App() {
+
+  const meusItens = ['React', 'Vue', 'Angular']
+
   return (
     <div className="App">
-      <h1>Renderização Condicional</h1>
-      <Condicional />
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens} />
+      <OutraLista itens={''} />
     </div>
   );
 }
 
-export default App;
+export default App
