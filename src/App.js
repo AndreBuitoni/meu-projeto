eea49590cd9c042ffe331114bd4ddcdd33f0464d
André Buitoni <a.buitoni@gmail.com>
2023-03-24 16:38:39 -0300

    Initiated Routes Lecture

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import {useState} from 'react'
-
 import './App.css'
-import Saudacao from './components/Saudacao';
-import SeuNome from './components/SeuNome';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+
+import Home from './pages/Home'
+import Empresa from './pages/Empresa'
+import Contato from './pages/Contato'
 
 function App() {
-  const [nome, setNome] = useState()
 
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome} />
-      <Saudacao nome={nome} />
-    </div>
-  );
+    <Router>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/empresa">Empresa</Link></li>
+        <li><Link to="/contato">Contato</Link></li>
+      </ul>
+    </Router>
+  )
 }
 
 export default App
